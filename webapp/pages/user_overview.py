58bb1d5e57046d2853c3ffbf3de17e0b45529659
Lucas Morgan <enviraldesign@gmail.com>
2024-05-26 13:32:10 -0400

    Added global metrics and updated trade group data structure in user overview page.

--- webapp/pages/user_overview.py
+++ webapp/pages/user_overview.py
@@ -21,13 +21,24 @@ df = load_data(file_path)
 # Create a DataFrame for the trade groups
 trade_groups = []
 
+# global metrics
+total_trades = 0
+total_net_profit = 0
+
+
 for group in config['trade-groups']:
     title = group['title']
     comment_filter = group['comment-filter']
     magic_filter = group['magic-filter']
     filtered_df = filter_dataframe(df, magic_filter, comment_filter)
     net_profit_sum = calculate_net_profit_sum(filtered_df)
-    trade_groups.append({'Title': title, 'Net Profit Sum': net_profit_sum})
+
+    data = {
+        'Title': title, 
+        '$ P/L': net_profit_sum,
+    }
+
+    trade_groups.append(data)
 
 trade_groups_df = pd.DataFrame(trade_groups)
 
