e8ecdcb7b084e72171326e694822b1cac3363a79
LUCAS M MORGAN <enviraldesign@gmail.com>
2024-06-12 06:16:23 -0500

    refactoring

--- webapp/PYINSTALLER_BUILD.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d %~dp0
-pyinstaller EFX_Dashboard.spec --noconfirm --clean
-pause
--- webapp/app.py
+++ webapp/app.py
@@ -1,16 +1,27 @@
 import streamlit as st
 from menu import menu
+import sys
 
 st.set_page_config(layout="wide")  # Set the page layout to wide
 
-hide_st_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;} I
-                </style>
-                """
+# hide_st_style = """
+#                 <style>
+#                 #MainMenu {visibility: hidden;}
+#                 footer {visibility: hidden;} I
+#                 </style>
+#                 """
 
-st.markdown(hide_st_style, unsafe_allow_html=True)
+# st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# st write the directory of this application
+# st.write(__file__)
+# sys.exit()
+
+# write the entire directory tree of files in the current directory
+# import os
+# for root, dirs, files in os.walk("."):
+#     for file in files:
+#         st.write(os.path.join(root, file))
 
 # Initialize st.session_state.role to None
 if "role" not in st.session_state:
